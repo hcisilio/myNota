99fb288ad743a7e98fbcb2d3c701f281c7a1a340
hcisilio <h.cisilio@live.com>
2014-09-04 22:23:02 -0300

    login layout bootstrap sem ações

--- GUIs/login.php
+++ GUIs/login.php
@@ -2,60 +2,90 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>CS MyNota - Login</title>
-	<link rel="stylesheet" type="text/css" href="CSS/mynota.css">
+	<link rel="stylesheet" type="text/css" href="CSS/estilos.css">
+	<link href="CSS/bootstrap.css" rel="stylesheet">	
+	<script src="../Ajax/jQuery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 	<script>
-		function limpar(texto){
-			var txt = texto;
-			if (txt == "Login"){
-				document.getElementById("id").value = "";
+	function logar() {		
+	    $.ajax({	    
+			type: "POST",
+			url: "../Controladores/controlador.php",
+			data: { 
+				classe: "Professor",
+				metodo: "login",
+				id: $("#id").val(),
+				senha: $("#senha").val()
+			},
+			
+			beforeSend: function() {
+				// enquanto a função esta sendo processada, você
+				// pode exibir na tela uma
+				// msg de carregando							
+			},
+			success: function(result) {
+				// pego o id da div que envolve o select com
+				// name="id_modelo" e a substituiu
+				// com o texto enviado pelo php, que é um novo
+				//select com dados da marca x
+				//$('#ajax_alunos').html(txt);
+				if (!result) {
+					$("#login").show("slow");
+					return false;
+				}
+				else {
+					window.location.href = result;
+				}				
+			},
+			error: function(txt) {
+				
 			}
-			if (txt=="Senha"){
-				document.getElementById("senha").value = "";
-			}
-		}
-		
-		function preencher(texto){
-			var txt = texto;
-			if (txt == ""){
-				document.getElementById("id").value = "Login";
-			}
-			if (txt==""){
-				document.getElementById("senha").value = "Senha";
-			}
-		}
-		
-		function viraSenha(elemento) {
-			elemento.type = "password";
-		}
-		
-		function viraText(elemento){
-			if (elemento.value == "") {
-				elemento.type = "text";
-			}
-		}
+	    }); 
+	}
+	function teste(){
+		window.location.href = "homeDiretor.php";
+	}	
 	</script>
 </head>
 <body>
-	<div id="principal">
-		<div id="topo"> <a href="index.html"> <img src="Imagens/logo.png" alt="logo" width="273" height="103"/> </a> </div>
+	<div id="row">
+		<div class="col-md-12 topo">
 		
-			<form action="../Controladores/controlador.php" method="post">  
-				<ul id="barra"> 
-					<li></li>
-				</ul>
-				
-				<div align="center" id="login">
-					<input type="hidden" name="classe" value="Professor">
-					<input type="hidden" name="metodo" value="login"> 
-					<p> <input name="id" type="text" size="15" class="edits" id ="id" value="Login" onFocus="JavaScript:limpar(this.value)" onBlur="JavaScript:preencher(this.value)"></p>
-					<p> <input name="senha" type="text" size="15" class="edits" id="senha" value="Senha" onFocus="JavaScript:viraSenha(this); JavaScript:limpar(this.value)" onBlur="JavaScript:viraText(this); JavaScript:preencher(this.value)"></p>     
-					<p><input name="btnLogin" type="submit" class="botaoReal" id="botao" style="botao" value="Login"></p> 					
-				</div> 
-			    
-		    </form>
-		<div id="rodape">
-			<p>I CS - Cisilio's Sistemas &copy;2014 - Todos os direitos reservados I <a href="#"></a> </p>
 		</div>
 	</div>
+	<div id="row">
+		<!-- logotipo -->
+		<div class="col-md-5">
+		
+		</div>
+		<!-- formulário -->
+		<div class="col-md-5">
+			<form action="../Controladores/controlador.php" method="post" onsubmit="JavaScript:return logar()">
+				<input type="hidden" name="classe" id="classe" value="Professor">
+				<input type="hidden" name="metodo" id="metodo" value="login"> 
+				<div class="input-group abaixo">
+			 		<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>			 		
+					<input class="form-control" name="id" id="id" type="text" placeholder="Login">
+			  	</div>
+			  	<div class="input-group abaixo">
+			 		<span class="input-group-addon"><span class="glyphicon"><img src="Imagens/glyphicons_044_keys.png" style="width: 14px; height: 14px;"></span></span>			 		
+					<input class="form-control" name="senha" id="senha" type="password" placeholder="Senha">
+			  	</div>	
+			  	<input type="button" class="btn btn-default" id="btnSubmit" value="Login" onclick="logar()">
+			</form>	
+			<div class="alert alert-danger" role="alert" id="login" style="display:none">
+				Usuário ou senha inválidos
+			</div>			
+		</div>
+		<!-- sobra -->
+		<div class="col-md-2">
+				<input type="button" value="teste" onclick="login()">
+		</div>
+	</div>
+	<div id="rodape">
+		<div id="miolo">
+			<p>I CS - Cisilio's Sistemas &copy;2014 - Todos os direitos reservados I <a href="#"></a> </p>			
+		</div>
+	</div>	
 </body>
 </html>
