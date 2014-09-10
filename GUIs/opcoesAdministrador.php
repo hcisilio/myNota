0cc2e21936018214e0c5e784dfeb6729827a65bf
Helber Cisilio <helber.santos@iff.edu>
2014-09-10 12:35:00 -0300

    Novo layout

--- GUIs/opcoesAdministrador.php
+++ GUIs/opcoesAdministrador.php
@@ -1,5 +1,6 @@
 <?php 
 echo "
+<a href='home.php' class='list-group-item active'> Página Inicial </a>
 <a href='registrarProfessor.php' class='list-group-item'>Registrar Novo Professor</a>
 <a href='registrarTurma.php' class='list-group-item'>Registrar Nova Turma</a>
 <a href='registrarAluno.php' class='list-group-item'>Matricular Aluno</a>
