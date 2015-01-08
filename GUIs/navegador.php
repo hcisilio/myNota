ea12d36b1ee50e70aac814ff0a63a27b6ce75c4e
Helber Cisilio <h.cisilio@live.com>
2015-01-08 15:36:53 -0200

    restrição de acesso pelo Chrome

--- GUIs/navegador.php
+++ GUIs/navegador.php
@@ -5,10 +5,6 @@
 	<link rel="stylesheet" type="text/css" href="CSS/estilos.css">
 	<link href="CSS/bootstrap.css" rel="stylesheet">
 	<style>
-		.proibido {
-			background: url('Imagens/proibido.png') no-repeat center center;
-			height: 80%;
-		}
 		.navbar-text {
 			color: #fff !important;			
 		}
@@ -23,11 +19,11 @@
 	<nav class="navbar navbar-default" role="navigation" id="barra">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<h1 class="navbar-text navbar-center"> Este sistema funciona apenas no google Chrome. Clique no ícone para fazer o download </h1>
+				<h1 class="navbar-text navbar-center"> Este sistema está disponível apenas no google Chrome. </h1>
 			</div>
 			<div class="collapse navbar-collapse">
 				<ul class="nav navbar-nav navbar-right">	
-					<li> <a href="http://www.google.com.br/chrome"> <img src="Imagens/Google_Chrome.png"> </a></li>				         
+					<li> </li>				         
 				</ul>
 			</div>
 		</div>
@@ -39,7 +35,8 @@
 		</div>
 		<!-- conteúdo -->
 		<div id="principal" class="abaixo col-md-10 proibido">				
-				
+			<div class='abaixo' align='center' > <a href="http://www.google.com.br/chrome"> <img src="Imagens/Google_Chrome.png"> </a> </div>
+			<div align='center' > Caso não possua o Google Chrome instalado, clique na imagem acima para fazer o download! </a> </div>
 		</div>
 		<!-- sobra -->
 		<div class="col-md-1">
