b1885ff7e584a23487cfc3428e39a609bad90fd4
Helber Cisilio <h.cisilio@live.com>
2015-01-09 16:56:13 -0200

    Criação de página administrativa para controle de acesso as páginas

--- Controladores/controladorPermissao.php
+++ Controladores/controladorPermissao.php
@@ -29,6 +29,72 @@ class ControladorPermissao {
 		}
 	}
 	
+	function inserir(){
+		$permissao = new Permissao();
+		$permissao->setPagina($_REQUEST["pagina"]);
+		$permissao->setDescricao($_REQUEST["descricao"]);
+		$permissao->setOrdem($_REQUEST["ordem"]);
+		$permissao->setNivel($_REQUEST["nivel"]);
+		$persistir = new PermissaoSQL();
+		if ($persistir->inserir($permissao)){
+			$resultado = "
+				<div class='alert alert-success' role='alert'>
+					Nova página registrada no sistema!
+				</div>
+			";
+		}
+		else {
+			$resultado = "
+				<div class='alert alert-danger' role='alert'>
+					Ops! Cadastro da páginanão efetuado. <BR />".mysql_error()."
+				</div>
+			";
+		}
+		echo $resultado;
+	}
+	
+	function alterar(){
+		$persistir = new PermissaoSQL();
+		$permissao = $persistir->listar($_REQUEST["pagina"]);
+		$permissao->setDescricao($_REQUEST["descricao"]);
+		$permissao->setOrdem($_REQUEST["ordem"]);
+		$permissao->setNivel($_REQUEST["nivel"]);
+		if ($persistir->alterar($permissao)){
+			$resultado = "
+				<div class='alert alert-success' role='alert'>
+					Configurações da página alterada com sucesso! (:
+				</div>
+			";
+		}
+		else {
+			$resultado = "
+				<div class='alert alert-danger' role='alert'>
+					Erro! Não foi possível gravar as alterações na página ): <BR />".mysql_error()."
+				</div>
+			";
+		}
+		echo $resultado;
+	}
+	
+	function deletar() {
+		$persistir = new PermissaoSQL();
+		if ($persistir->deletar($_REQUEST["pagina"])){
+			$resultado = "
+				<div class='alert alert-success' role='alert'>
+					Página ".$_REQUEST["pagina"]." removida do sistema!
+				</div>
+			";
+		}
+		else {
+			$resultado = "
+				<div class='alert alert-danger' role='alert'>
+					Ops! Página ".$_REQUEST["pagina"]." não pode ser removida <BR />".mysql_error()."
+				</div>
+			";
+		}
+		echo $resultado;
+	}
+	
 	function autorizarAcesso($pagina){
 		//consulta o nível de permissão da página atual
 		$persistir = new PermissaoSQL();
@@ -46,6 +112,27 @@ class ControladorPermissao {
 			$menu .= "<a href='../GUIs/".$pagina->getPagina()."' class='list-group-item'>".$pagina->getDescricao()."</a>";
 		}
 		echo $menu;
+	}
+	
+	function gerenciarPaginas(){
+		$n = 0;
+		$persistir = new PermissaoSQL();
+		foreach ($persistir->listarTodos() as $pagina) {
+			$form = "
+				<tr>
+					<form id='paginas$n' class='form-inline' role='form' method='GET'>					
+						<td><input id='pagina$n' class='edits nuloOUvazio' type='text' value='".$pagina->getPagina()."' readonly></td>
+						<td><input id='descricao$n' class='edits nuloOUvazio' type='text' value='".$pagina->getDescricao()."'></td>
+						<td><input id='nivel$n' class='edits nuloOUvazio' size='10' type='text' value=".$pagina->getNivel()."></td>
+						<td><input id='ordem$n' class='edits nuloOUvazio' size='10' type='text' value=".$pagina->getOrdem()."></td>
+						<td><a href='JavaScript:atualizar($n)'> <img src='Imagens/save.png' height='28'> </a></td>
+						<td><a href='JavaScript:deletar($n)'> <img src='Imagens/delete-icon.png' height='28'> </a></td>
+					</form>
+				</tr>
+			";
+			echo $form;
+			$n++;
+		}
 	}
 	
 }
---
+++ GUIs/gerenciarPaginas.php
@@ -0,0 +1,172 @@
+<?php 
+	include_once ("../Controladores/controladorPermissao.php");
+	$persistir = new ControladorPermissao();
+	$persistir->autorizarAcesso( end(explode("/", $_SERVER['PHP_SELF'])) );
+?>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   	<title>Gerenciamento de acesso as páginas</title>
+   	<link rel="stylesheet" type="text/css" href="CSS/estilos.css">
+	<link href="CSS/bootstrap.css" rel="stylesheet">	
+	<script src="../Ajax/jQuery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../Ajax/validacoes.js"></script>
+	<script>
+		function registrar() {
+		   if ( nuloOUvazio("#paginas") ) {
+		       $.ajax({			        
+					type: "POST",
+					url: "../Controladores/controlador.php",
+					data: { 
+						pagina: $("#pagina").val(),
+						descricao: $("#descricao").val(),
+						nivel: $("#nivel").val(),
+						ordem: $("#ordem").val(),
+						classe: "Permissao",
+						metodo: "inserir" 
+					},
+					success: function(resultado) {
+						location.reload();
+						$('#alerta').hide();				
+						$('#alerta').html(resultado);
+						$('#alerta').show("slow");
+					},
+					error: function() {				
+						$('#alerta').html('fudeu');
+					}
+			    });
+		   }
+	   	}
+		function atualizar(linha) {
+		   if ( nuloOUvazio("#paginas"+linha) ) {			   
+		       $.ajax({			        
+					type: "POST",
+					url: "../Controladores/controlador.php",
+					data: { 
+						pagina: $("#pagina"+linha).val(),
+						descricao: $("#descricao"+linha).val(),
+						nivel: $("#nivel"+linha).val(),
+						ordem: $("#ordem"+linha).val(),
+						classe: "Permissao",
+						metodo: "alterar" 
+					},
+					success: function(resultado) {
+						$('#alerta').hide();				
+						$('#alerta').html(resultado);
+						$('#alerta').show("slow");
+					},
+					error: function(resultado) {				
+						
+					}
+			    });
+		   }
+	   	}
+		function deletar(linha){
+			$.ajax({			        
+				type: "POST",
+				url: "../Controladores/controlador.php",
+				data: { 
+					pagina: $("#pagina"+linha).val(),
+					classe: "Permissao",
+					metodo: "deletar" 
+				},
+				success: function(resultado) {
+					location.reload();
+					$('#alerta').hide();				
+					$('#alerta').html(resultado);
+					$('#alerta').show("slow");
+				},
+				error: function(resultado) {				
+					
+				}
+		    });
+		}
+  	</script>
+</head>
+<body>
+	<!-- NavBar -->
+	<nav class="navbar navbar-default" role="navigation" id="barra">
+		<div class="container-fluid">
+			<div class="navbar-header">	
+				<a class="navbar-brand" href="../GUIs/home.php"> <img alt="Brand" src="Imagens/sublogo_branco.png"> </a>
+			</div>
+			<div class="collapse navbar-collapse">
+			<form action="../Controladores/controlador.php" method="post" name="logout" id="logout">
+				<input type="hidden" name="classe" value="Professor">
+				<input type="hidden" name="metodo" value="logout">
+				<ul class="nav navbar-nav navbar-right">
+					<li> <a href="" data-toggle="modal" data-target="#registrarNovaPagina"> <img src="Imagens/new-page.png"> </a></li>
+				</ul>
+			</form>
+			</div>
+		</div>
+	</nav>
+	
+	<!-- Modal -->
+	<div class="modal fade" id="registrarNovaPagina" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+		 <div class="modal-content">
+		   <div class="modal-header">
+		     <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+		     <h4 class="modal-title" id="myModalLabel">Registrar nova página</h4>
+		   </div>
+		   <div class="modal-body">
+		   	<table border='1' class='table table-striped tabela-consulta'>
+				<tr>
+					<th>Nome do Arquivo</th>
+					<th>Descrição</th>
+					<th>Nível de acesso</th>
+					<th>Ordem no menu</th>
+					<th>  </th>
+				</tr>
+				<form id='paginas' class='form-inline' role='form' method='GET'>
+				<div class="input-group abaixo"> 		
+					<td><input id='pagina' class='edits nuloOUvazio' type='text' placeholder="Nome do arquivo .php"></td>
+					<td><input id='descricao' class='edits nuloOUvazio' type='text' placeholder="Descriçao para o menu"></td>
+					<td><input id='nivel' class='edits nuloOUvazio' size='10' type='text' placeholder="Nível de acesso a página"></td>
+					<td><input id='ordem' class='edits nuloOUvazio' size='10' type='text' placeholder="Ordem no menu"></td>
+					<td><a href='JavaScript:registrar()'> <img src='Imagens/save.png' height='28'> </a></td>								  	
+			  	</div>			  	
+				</form>
+			 </table>			
+		   </div>
+		 </div>
+	  </div>
+	</div>
+	
+	<div id="row">
+		<!-- menu lateral -->
+		<div class="col-md-3 menuLateral">
+			<div class="list-group">				
+			  	<?php $persistir->criarMenu() ?>
+			</div>
+		</div>
+		<!-- espaçamento -->
+		<div class="col-md-1">
+		</div>
+		<!-- conteúdo -->
+		<div class="col-md-7 abaixo">	
+			<div id='alerta'></div>
+			<table border='1' class='table table-striped tabela-consulta'>
+				<tr>
+					<th>Nome do Arquivo</th>
+					<th>Descrição</th>
+					<th>Nível de acesso</th>
+					<th>Ordem no menu</th>
+					<th>  </th>
+					<th>  </th>
+				</tr>
+				<?php $persistir->gerenciarPaginas() ?>
+			</table>	
+		</div>	
+		<!-- sobra -->
+		<div class="col-md-1">
+		</div>
+	</div>
+	
+	<!-- rodapé -->
+	<?php include ("rodape.php") ?>
+
+</body>
+</html>
